d93cb77abe5fb56156394c239741174088970a49
koenvanderstukken <koen.vanderstukken@hotmail.com>
2019-01-18 08:53:44 +0100

    Chromedriver koen

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -22,7 +22,9 @@ public class StepDefinitions {
     // GENERAL
     @Given("I launch Chrome browser")
     public void i_Launch_Chrome_Browser(){
-       System.setProperty("webdriver.chrome.driver","C:\\Users\\caglarcelikoz\\Downloads\\chromedriver.exe");
+       //System.setProperty("webdriver.chrome.driver","C:\\Users\\caglarcelikoz\\Downloads\\chromedriver.exe");
+       System.setProperty("webdriver.chrome.driver","C:\\Users\\Windows\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
 
         webDriver = new ChromeDriver();
     }
