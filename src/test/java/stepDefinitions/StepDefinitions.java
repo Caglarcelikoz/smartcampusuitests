c37c8b5384de66705dc548e7e924d299d41c82db
caglarcelikoz <caglarcelikoz@outlook.be>
2019-01-21 22:58:54 +0100

    test

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -1,11 +1,9 @@
 package stepDefinitions;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.json.simple.JSONObject;
-import org.junit.After;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
